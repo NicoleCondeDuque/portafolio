c1a284d9aee972768319eec47144b0c3a6eb15bb
unknown <nicolecondeduque@gmail.com>
2023-05-13 20:01:44 -0500

    se agregan secciones de header, home skills del portafolio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
-import Header from "./components/Header";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
 
 function App() {
   return (
     <>
       <Header />
+      <main className="main">
+        <Home />
+      </main>
+      <About />
+      <Skills />
     </>
   );
 }
