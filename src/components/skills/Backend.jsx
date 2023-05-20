39f24d56d0e9746f38bbc02573abde5b27f3b425
unknown <nicolecondeduque@gmail.com>
2023-05-20 16:25:07 -0500

    change the name yo NPM

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -29,7 +29,7 @@ const Backend = () => {
           <div className="skills__data">
             {/*<HiCheckBadge />*/}
             <div>
-              <h3 className="skills__name">npm</h3>
+              <h3 className="skills__name">NPM</h3>
               <span className="skills__level">Basic</span>
             </div>
           </div>
