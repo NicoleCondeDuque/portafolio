e85ce4784fd3dc2b854462024e7dea3cdd1eb7be
NicoleCondeDuque <nicolecondeduque@gmail.com>
2024-01-09 11:42:52 -0700

    act and modifications

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -11,7 +11,7 @@ const Info = () => {
       <div className="about__box">
         {/*<HiOutlineDesktopComputer className="about__icon" />*/}
         <h3 className="about__title">Experience</h3>
-        <span className="about__subtitle">1 Year</span>
+        <span className="about__subtitle">2 Year</span>
       </div>
       <div className="about__box">
         {/*<HiOutlineTerminal className="about__icon" />*/}
