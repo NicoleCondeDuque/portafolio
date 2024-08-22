4766ebab63c2d08404445fb21f60c041d61e84d5
NicoleCondeDuque <nicolecondeduque@gmail.com>
2024-08-22 12:09:43 -0600

    add about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
         <div className="about__data">
           <Info />
           <p className="about__description">
-          Welcome to my website! As a software developer with experience in product management, I help create digital solutions. My background includes developing products with a focus on analytical problem-solving, time management, and the ability to work both in groups and independently. I am committed to continuing to learn. Thank you for visiting my portal, a creative space that will show you a bit more about me. Feel free to contact me! ✨
+          Welcome to my professional space! As a software developer with hands-on experience in product management, I specialize in turning complex challenges into streamlined digital solutions. My work is rooted in a deep understanding of analytical problem-solving, efficient time management, and the ability to drive results both independently and within a team. I am constantly evolving, committed to learning and adapting in this fast-paced industry. This space reflects my journey, my skills, and my dedication to excellence. I'm excited to share my work with you—let’s connect and explore how I can contribute to your next project. ✨
           </p>
           <a download="" href={CV} className="button button--flex">
             Download CV
