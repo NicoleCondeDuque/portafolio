eeb9a8cc66e00b3f8ff34c539f0dfdb6957c7613
NicoleCondeDuque <nicolecondeduque@gmail.com>
2025-01-22 13:25:16 -0700

    adding style in the about me button

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -24,9 +24,12 @@ const About = () => {
 
  <br /><br />
   Currently, I'm working on a project to support the Latin community in Calgary: <b>a driving practice test to help Spanish speakers prepare for their driver's license exam</b>. 
-  If you’d like to check it out, you can visit it here: 
+  If you’d like  you can check it out by clicking the button below.
+  <br /><br />
+<b> Your feedback is always welcome!</b>
+           <br /><br />
+Take a look around, and if you think we can create something amazing together, <b>let’s connect! 🚀✨</b>
 </p>
-
           <a href="https://driver-test-hazel.vercel.app/welcome" target="_blank" rel="noopener noreferrer" className="button button--flex">
           Driver Test
             <svg
@@ -37,10 +40,6 @@ const About = () => {
               viewBox="0 0 24 24"
               fill="none"
             >
-              <path
-                d="M15.25 22.7502H9.25C3.82 22.7502 1.5 20.4302 1.5 15.0002V9.00024C1.5 3.57024 3.82 1.25024 9.25 1.25024H14.25C14.66 1.25024 15 1.59024 15 2.00024C15 2.41024 14.66 2.75024 14.25 2.75024H9.25C4.64 2.75024 3 4.39024 3 9.00024V15.0002C3 19.6102 4.64 21.2502 9.25 21.2502H15.25C19.86 21.2502 21.5 19.6102 21.5 15.0002V10.0002C21.5 9.59024 21.84 9.25024 22.25 9.25024C22.66 9.25024 23 9.59024 23 10.0002V15.0002C23 20.4302 20.68 22.7502 15.25 22.7502Z"
-                fill="var(--container-color)"
-              ></path>
               <path
                 d="M22.25 10.7502H18.25C14.83 10.7502 13.5 9.42023 13.5 6.00023V2.00023C13.5 1.70023 13.68 1.42023 13.96 1.31023C14.24 1.19023 14.56 1.26023 14.78 1.47023L22.78 9.47023C22.99 9.68023 23.06 10.0102 22.94 10.2902C22.82 10.5702 22.55 10.7502 22.25 10.7502ZM15 3.81023V6.00023C15 8.58023 15.67 9.25023 18.25 9.25023H20.44L15 3.81023Z"
                 fill="var(--container-color)"
@@ -83,11 +82,8 @@ const About = () => {
               ></path>
             </svg>
           </a> */}
-           <p className="mt-4">Your feedback is always welcome!
-
-           <br /><br />
-Take a look around, and if you think we can create something amazing together, <b>let’s connect! 🚀✨</b>
-        </p>
+    
+    
         </div>
       </div>
     </section>
